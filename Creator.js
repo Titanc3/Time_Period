626075c6093174a416330e7f1b5bf2febdd7ddb0
Titanc3 <123788674+Titanc3@users.noreply.github.com>
2023-04-25 16:39:06 -0400

    Update Creator.js

--- Creator.js
+++ Creator.js
@@ -1,9 +1,10 @@
 function submit() {
+  // REQUIRED: button with an id of 'sumbitButton' with an onclick of 'submit()', and 3 str inputs with ids of names, startTime, and endTime (input must be separated by a space)
   let webpage = "" // insert the webpage that uses the runner
   let symbols = "∆ ∇ ∎ ≡ ≣ ⊏ ⊐ ⊑ ⊒ ⊕ ⊖ ⊗ ⊘ ⊙ ⊚ ⊛ ⊜ ⊝ ⊞ ⊟ ⊠ ⊡ ⊿ ⋄ ⋐ ⋑ ⋒ ⋓ ⍁ ⍂ ⎔ □ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▪ ▫ ▬ ▭ ▮ ▯ ▰ ▱ ▲ △ ▴ ▵ ▶ ▷ ▸ ▹ ▻ ▼ ▽ ▾ ▿ ◀ ◁ ◂ ◃ ◅ ◆ ◇ ◈ ◉ ◊ ○ ◌ ◍ ◎ ● ◐ ◑ ◒ ◓ ◔ ◕ ◖ ◗ ◧ ◨ ◩ ◪ ◫ ◬ ◭ ◮ ◯ ◰ ◱ ◲ ◳ ◴ ◵ ◶".split(" ");
-  let names = document.getElementById("names").value.split(" ");
-  let startTime = document.getElementById("startTime").value.split(" ");
-  let endTime = document.getElementById("endTime").value.split(" ");
+  let names = document.getElementById("names").value.split(" "); // the name of each time period
+  let startTime = document.getElementById("startTime").value.split(" "); // the start time of each period in 24hr time/millitary time, smallest->largest
+  let endTime = document.getElementById("endTime").value.split(" "); //same as start times but the end times
   let url = `${webpage}?title=${document.getElementById("title").value}&names=${names.join("|")}&m=`;
 
   if (startTime.length != endTime.length) {alert("Your start times and end times don't match");}
