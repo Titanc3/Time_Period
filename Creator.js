5da0b31ade1882c0dd8b84b493c1cf490dba468a
Titanc3 <123788674+Titanc3@users.noreply.github.com>
2023-04-25 16:27:06 -0400

    Update Creator.js

--- Creator.js
+++ Creator.js
@@ -1,1 +1,20 @@
+function submit() {
+  let webpage = "" // insert the webpage that uses the runner
+  let symbols = "∆ ∇ ∎ ≡ ≣ ⊏ ⊐ ⊑ ⊒ ⊕ ⊖ ⊗ ⊘ ⊙ ⊚ ⊛ ⊜ ⊝ ⊞ ⊟ ⊠ ⊡ ⊿ ⋄ ⋐ ⋑ ⋒ ⋓ ⍁ ⍂ ⎔ □ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▪ ▫ ▬ ▭ ▮ ▯ ▰ ▱ ▲ △ ▴ ▵ ▶ ▷ ▸ ▹ ▻ ▼ ▽ ▾ ▿ ◀ ◁ ◂ ◃ ◅ ◆ ◇ ◈ ◉ ◊ ○ ◌ ◍ ◎ ● ◐ ◑ ◒ ◓ ◔ ◕ ◖ ◗ ◧ ◨ ◩ ◪ ◫ ◬ ◭ ◮ ◯ ◰ ◱ ◲ ◳ ◴ ◵ ◶".split(" ");
+  let names = document.getElementById("names").value.split(" ");
+  let startTime = document.getElementById("startTime").value.split(" ");
+  let endTime = document.getElementById("endTime").value.split(" ");
+  let url = `${webpage}?title=${document.getElementById("title").value}&names=${names.join("|")}&m=`;
 
+  if (startTime.length != endTime.length) {alert("Your start times and end times don't match");}
+  else {
+    for (let i = 0; i < startTime.length; i++) {
+      let startList = startTime[i].split(":");
+      let endList = endTime[i].split(":");
+      if (i == 0) { url += symbols[parseInt(startList[0])] + symbols[parseInt(startList[1])] + "|" + symbols[parseInt(endList[0])] + symbols[parseInt(endList[1])]; }
+      else { url += "|" + symbols[parseInt(startList[0])] + symbols[parseInt(startList[1])] + "|" + symbols[parseInt(endList[0])] + symbols[parseInt(endList[1])]; }
+    }
+  }
+  document.getElementById("submitButton").innerHTML = url;
+  alert("Your personalized URL is "+url);
+  }
